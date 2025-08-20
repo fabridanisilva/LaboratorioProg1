e16c39cf85328e815044076fce7d917fb9ee56da
fabridanisilva <fabridanisilva@gmail.com>
2025-08-20 19:57:10 -0300

    Add files via upload

---
+++ Estudiante.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package RegistroEstudiantes;
+
+/**
+ *
+ * @author fabri
+ */
+public class Estudiante {
+    String nombre;
+    String apellido;
+    double promedio;
+
+    public Estudiante(String nombre, String apellido, double promedio) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.promedio = promedio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "nombre=" + nombre + ", apellido=" + apellido + ", promedio=" + promedio + '}';
+    }
+    
+    
+}
